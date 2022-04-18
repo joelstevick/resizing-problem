0ba8ae7eb1d4cb237516379bf88a786be8473abf
Joel Stevick <joelstevick@outlook.com>
2022-04-18 15:26:39 -0400

    Initial Commit

---
+++ Resizing-Problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resizing-Problem
+//
+//  Created by Joel Stevick on 4/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
